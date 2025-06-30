3e823203856e766217748dafb0cc85468109d4bc
ayesorobolu <abrahamayesoro@gmail.com>
2025-06-30 18:00:15 +0100

    feat: configured tailwind css feat: import font and added color scheme

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <h1 className="text-3xl font-bold underline text-center mt-10">
+        hello world
+      </h1>
     </div>
   )
 }
