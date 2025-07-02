8f834953219bbd6af3b03dee0854396d6cbe62db
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-07-02 19:08:23 +0100

    feat: add Banner component

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import FeaturedSection from '../Components/FeaturedSection'
+import Banner from '../Components/Banner'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <FeaturedSection/>
+        <Banner/>
     </div>
   )
 }
