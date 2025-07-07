ca03344b928421de362236cd18fed6dc06f749f7
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-07-07 19:22:14 +0100

    feat: implement database connection logic using mongoose
    - Created async connectDB function
    - Added event listener for successful connection
    - Used environment variable for MongoDB URI
    - Exported connectDB for use in main app

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import "dotenv/config";
 import cors from 'cors';
+import connectDB from './configs/db.js';
 
 const PORT = process.env.PORT || 3000;
 // Initialize express App 
 const app = express()
+
+//connect database
+await connectDB()
 
 // Middleware 
 app.use(cors())
