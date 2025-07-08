9cb608a1212550e44bcb5e982ed37ba9fd80d416
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-07-08 15:50:29 +0100

    feat(auth): implement JWT token generation and error handling in user registration
    - Imported jsonwebtoken and created a generateToken function
    - Signed token using user's ID and secret key from .env
    - Returned token in successful registration response
    - Added structured error logging and JSON error response

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,6 +1,14 @@
 import User from "../models/User.js"
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
+//Generate JWT Token
+const generateToken = (userId) => {
+    const payload = userId;
+    return jwt.sign(payload, process.env.JWT_SECRET)
+}
+
+//Register User
 export const registerUser = async (req, res) => {
     try {
         const {name, email, password} = req.body
@@ -16,7 +24,10 @@ export const registerUser = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10)
         const user = await User.create({name, email, password: hashedPassword})
+        const token = generateToken(user._id.toString())
+        res.json({succes:true, token})
     } catch (error) {
-        
+        console.log(error.message);
+        res.json({success: false, message: error.message})
     }
 }
