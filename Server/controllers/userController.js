562dc4d22bb74dc6c21c523ff95cd1565c997844
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-07-08 15:56:35 +0100

    feat(auth): implement user login with password verification and JWT token generation
    - Validates email and password from request
    - Compares password with hashed version using bcrypt
    - Generates JWT token if credentials are valid
    - Returns structured success or error response

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -24,6 +24,25 @@ export const registerUser = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10)
         const user = await User.create({name, email, password: hashedPassword})
+        
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+//Login User
+export const loginUser = async (req,res) =>{
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if (!user){
+            return res.json({success:false, message: "User not found"})
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.json({success:false, message: "Invalid Credentials"})
+        }
         const token = generateToken(user._id.toString())
         res.json({succes:true, token})
     } catch (error) {
