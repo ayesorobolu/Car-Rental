511b1aa2223100965dddf224beca465fe0907953
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-07-10 00:02:58 +0100

    feat: add getOwnerBookings controller to fetch all bookings associated with the authenticated owner

--- Server/controllers/bookingController.js
+++ Server/controllers/bookingController.js
@@ -66,9 +66,23 @@ export const createBooking = async (req, res) =>{
 //API to list User booking
 export const getUserBookings =  async (req,res) => {
     try {
-        const {_id} = req.user;""
+        const {_id} = req.user;
         const bookings = await Booking.find({user: _id}).populate("car").sort({createdAt: -1})
         res.json({success: true, bookings})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+//API to get owner Bookings
+export const getOwnerBookings =  async (req,res) => {
+    try {
+       if(req.user.role !== 'owner'){
+        return res.json({success: false, message: "Unauthorized"})
+       }
+       const bookings =  await Booking.find({owner: req.user._id}).populate('car user').select("-user.password").sort({createdAt: -1})
+       res.json({success: true, bookings})
     } catch (error) {
         console.log(error.message);
         res.json({success: false, message: error.message})
