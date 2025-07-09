02892db2dbff72edf4053ef774f50c3bc59660b5
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-07-09 19:46:56 +0100

    feat: add function to retrieve cars listed by the owner

--- Server/controllers/ownerController.js
+++ Server/controllers/ownerController.js
@@ -50,3 +50,15 @@ export const addCar =  async (req,res) => {
         res.json({success: false, message: error.message})
     }
 } 
+
+//API to list owner cars
+export const getOwnerCars = async (req,res) => {
+    try {
+        const {_id} = req.server
+        const cars = await Car.find({owner: _id})
+        res.json({success: true, cars})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
