a6fbea9c4f85215abe69fd7c03b89d5ce636c4ef
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-07-09 22:31:05 +0100

    feat: add toggleCarAvailability function to allow owners to toggle car availability status

--- Server/controllers/ownerController.js
+++ Server/controllers/ownerController.js
@@ -45,6 +45,28 @@ export const addCar =  async (req,res) => {
        res.json({succes: true, cars})
 
     } catch (error) {
+        console.log(error.message); b 
+        res.json({success: false, message: error.message})
+    }
+} 
+
+//API to toggle Car Availability
+export const toggleCarAvailability = async (req,res) => {
+    try {
+        const {_id} = req.user;
+        const {carId} = req.body
+        const car = await Car.find(carId )
+         
+        //Checking if car belongs to User
+        if(car.owner.toString() !== _id.toString()){
+            return res.json({success: false, message: "Unauthorized"});
+        }
+
+        car.isAvaliable = !car.isAvaliable;
+        await car.save()
+
+        res.json({success: true, message:"Availability Toggled"})
+    } catch (error) {
         console.log(error.message);
         res.json({success: false, message: error.message})
     }
