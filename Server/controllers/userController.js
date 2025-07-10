90e44a0f403d5a98bb2ef07e9277a90847faa8fa
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-07-10 23:29:11 +0100

    feat: add getCars controller to fetch available cars for frontend

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
+import Car from "../models/Car.js";
 
 //Generate JWT Token
 const generateToken = (userId) => {
@@ -61,3 +62,14 @@ export const getUserData = async (req,res)=> {
     res.json({success: false, message: error.message})
  }
 }
+
+//get all Cars for frontend
+export const getCars = async (req,res)=> {
+    try {
+      const cars = await Car.find({isAvaliable: true})
+      res.json({success: true, cars})
+    } catch (error) {
+       console.log(error.message);
+       res.json({success: false, message: error.message})
+    }
+   }
