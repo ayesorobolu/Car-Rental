2def94a5a6ae7a53fbfb19ed5b8c3a8915dffdce
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-07-10 21:34:10 +0100

    feat: add API to update user profile image with ImageKit optimization

--- Server/controllers/ownerController.js
+++ Server/controllers/ownerController.js
@@ -142,3 +142,39 @@ export const getDashboardData = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+
+//API to update user image
+export const updateUserImage = async (req, res) => {
+    try {
+        const {_id} = req.user;
+
+        const imageFile = req.file;
+
+        //Upload Image to ImageKit
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        const response = await imagekit.upload({
+         file: fileBuffer,
+         fileName: imageFile.originalname,
+         folder: '/users'
+       })
+
+        // optimization through imagekit URL transformation
+        var optimizedImageUrl = imagekit.url({
+            path : response.filePath,
+            transformation : [
+                {width: '400'}, //width resize  
+                {quality: 'auto'}, //auto compression
+                {format: 'webp'} //convert to modern format
+            ]
+           });
+
+           const image = optimizedImageUrl;
+
+           await User.findByIdAndUpdate(_id, {image});
+           res.json({succes: true, message:"Image Updated"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
